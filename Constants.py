73903939703faee89a46275826dd5176ee10d66a
Guzaiwang <guzaiwang01@gmail.com>
2019-03-17 23:44:53 +0800

    First submission:
    1. Dataloader
    2. Framework: Data augmentation (training and test)
    3. Loss: binary dice loss and multi-class dice loss
    4. Network: CE-Net

--- Constants.py
+++ Constants.py
@@ -1,1 +1,10 @@
 import os
+import shutil
+
+Image_size = (448, 448)
+ROOT = './dataset/DRIVE'
+BATCHSIZE_PER_CARD = 8
+TOTAL_EPOCH = 300
+INITAL_EPOCH_LOSS = 10000
+NUM_EARLY_STOP = 20
+NUM_UPDATE_LR = 10
---
+++ main.py
@@ -0,0 +1,100 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import cv2
+import os
+from time import time
+
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+from torch.autograd import Variable as V
+
+from networks.cenet import CE_Net_
+from framework import MyFrame
+from loss import dice_bce_loss
+from data import ImageFolder
+from Visualizer import Visualizer
+
+import Constants
+
+# Please specify the ID of graphics cards that you want to use
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+
+
+def CE_Net_Train():
+    NAME = 'CE-Net' + Constants.ROOT.split('/')[-1]
+
+    # run the Visdom
+    viz = Visualizer(env=NAME)
+
+    solver = MyFrame(CE_Net_, dice_bce_loss, 2e-4)
+    batchsize = torch.cuda.device_count() * Constants.BATCHSIZE_PER_CARD
+
+    # For different 2D medical image segmentation tasks, please specify the dataset which you use
+    # for examples: you could specify "dataset = 'DRIVE' " for retinal vessel detection.
+
+    dataset = ImageFolder(root_path=Constants.ROOT, datasets='DRIVE')
+    data_loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batchsize,
+        shuffle=True,
+        num_workers=4)
+
+    # start the logging files
+    mylog = open('logs/' + NAME + '.log', 'w')
+    tic = time()
+
+    no_optim = 0
+    total_epoch = Constants.TOTAL_EPOCH
+    train_epoch_best_loss = Constants.INITAL_EPOCH_LOSS
+    for epoch in range(1, total_epoch + 1):
+        data_loader_iter = iter(data_loader)
+        train_epoch_loss = 0
+        index = 0
+
+        for img, mask in data_loader_iter:
+            solver.set_input(img, mask)
+            train_loss, pred = solver.optimize()
+            train_epoch_loss += train_loss
+            index = index + 1
+
+        # show the original images, predication and ground truth on the visdom.
+        show_image = (img + 1.6) / 3.2 * 255.
+        viz.img(name='images', img_=show_image[0, :, :, :])
+        viz.img(name='labels', img_=mask[0, :, :, :])
+        viz.img(name='prediction', img_=pred[0, :, :, :])
+
+        train_epoch_loss = train_epoch_loss/len(data_loader_iter)
+        print(mylog, '********')
+        print(mylog, 'epoch:', epoch, '    time:', int(time() - tic))
+        print(mylog, 'train_loss:', train_epoch_loss)
+        print(mylog, 'SHAPE:', Constants.Image_size)
+        print('********')
+        print('epoch:', epoch, '    time:', int(time() - tic))
+        print('train_loss:', train_epoch_loss)
+        print('SHAPE:', Constants.Image_size)
+
+        if train_epoch_loss >= train_epoch_best_loss:
+            no_optim += 1
+        else:
+            no_optim = 0
+            train_epoch_best_loss = train_epoch_loss
+            solver.save('./weights/' + NAME + '.th')
+        if no_optim > Constants.NUM_EARLY_STOP:
+            print(mylog, 'early stop at %d epoch' % epoch)
+            print('early stop at %d epoch' % epoch)
+            break
+        if no_optim > Constants.NUM_UPDATE_LR:
+            if solver.old_lr < 5e-7:
+                break
+            solver.load('./weights/' + NAME + '.th')
+            solver.update_lr(2.0, factor=True, mylog=mylog)
+        mylog.flush()
+
+    print(mylog, 'Finish!')
+    print('Finish!')
+    mylog.close()
+
+
